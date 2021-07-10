7bd518b5bc7a84b00d2e49c9c691a17b755753ca
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-10 17:31:59 +0700

    refactor: Use tailwind config for the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ...fontFamily,
+        sans: ['Anakotmai'],
+        // body: ['Anakotmai'],
+        // display: ['Anakotmai'],
+      },
+    },
   },
   variants: {
     extend: {},
