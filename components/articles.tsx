dd10cde3dc5f81e494c8fb7e3666fb0ad87c631b
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-26 20:58:48 +0700

    feat: Add 3rd article

--- components/articles.tsx
+++ components/articles.tsx
@@ -6,7 +6,7 @@ export default function Articles() {
       <h2 className="text-3xl font-bold sm:text-6xl">บทความ</h2>
 
       <div className="flex mt-8">
-        <div className="shadow-lg mx-auto max-w-sm ">
+        <div className="shadow-lg mx-auto max-w-sm">
           <a
             href="https://narze.medium.com/%E0%B8%9A%E0%B8%B1%E0%B8%99%E0%B8%97%E0%B8%B6%E0%B8%81%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B9%81%E0%B8%9B%E0%B9%89%E0%B8%99%E0%B8%9E%E0%B8%B4%E0%B8%A1%E0%B8%9E%E0%B9%8C%E0%B9%84%E0%B8%97%E0%B8%A2-manoonchai-1-%E0%B8%AB%E0%B8%99%E0%B8%97%E0%B8%B2%E0%B8%87%E0%B8%AA%E0%B8%B9%E0%B9%88-layout-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%94%E0%B8%B5%E0%B8%81%E0%B8%A7%E0%B9%88%E0%B8%B2-8088ca83b1c3"
             target="_blank"
@@ -22,11 +22,10 @@ export default function Articles() {
                 บันทึกการสร้างแป้นพิมพ์ไทย Manoonchai (1) : หนทางสู่ Layout
                 ที่ดีกว่า
               </h3>
-              <p className="text-base text-gray-400 mt-2"></p>
             </div>
           </a>
         </div>
-        <div className="shadow-lg mx-auto max-w-sm ">
+        <div className="shadow-lg mx-auto max-w-sm">
           <a
             href="https://narze.medium.com/%E0%B8%9A%E0%B8%B1%E0%B8%99%E0%B8%97%E0%B8%B6%E0%B8%81%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B9%81%E0%B8%9B%E0%B9%89%E0%B8%99%E0%B8%9E%E0%B8%B4%E0%B8%A1%E0%B8%9E%E0%B9%8C%E0%B9%84%E0%B8%97%E0%B8%A2-manoonchai-2-%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-typing-effort-model-%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%83%E0%B8%AB%E0%B9%89%E0%B8%84%E0%B8%B0%E0%B9%81%E0%B8%99%E0%B8%99%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1-%E0%B8%9E%E0%B8%B4%E0%B8%A1%E0%B8%9E%E0%B9%8C%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2-ed729b67d52"
             target="_blank"
@@ -42,7 +41,25 @@ export default function Articles() {
                 บันทึกการสร้างแป้นพิมพ์ไทย Manoonchai (2) : สร้าง Typing Effort
                 Model เพื่อให้คะแนนความ “พิมพ์ง่าย”
               </h3>
-              <p className="text-base text-gray-400 mt-2"></p>
+            </div>
+          </a>
+        </div>
+        <div className="shadow-lg mx-auto max-w-sm">
+          <a
+            href="https://narze.medium.com/%E0%B8%9A%E0%B8%B1%E0%B8%99%E0%B8%97%E0%B8%B6%E0%B8%81%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B9%81%E0%B8%9B%E0%B9%89%E0%B8%99%E0%B8%9E%E0%B8%B4%E0%B8%A1%E0%B8%9E%E0%B9%8C%E0%B9%84%E0%B8%97%E0%B8%A2-manoonchai-3-%E0%B8%A7%E0%B8%B4%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%B0%E0%B8%AB%E0%B9%8C%E0%B9%81%E0%B8%9B%E0%B9%89%E0%B8%99%E0%B8%9E%E0%B8%B4%E0%B8%A1%E0%B8%9E%E0%B9%8C-manoonchai-%E0%B9%80%E0%B8%97%E0%B8%B5%E0%B8%A2%E0%B8%9A%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%A1%E0%B8%93%E0%B8%B5-%E0%B8%9B%E0%B8%B1%E0%B8%95%E0%B8%95%E0%B9%82%E0%B8%8A%E0%B8%95%E0%B8%B4-e4d9bd82f2ea"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {/* <Image
+              className="w-full h-auto"
+              src="https://place-hold.it/300x150?text=Placeholder&fontsize=23"
+              alt=""
+            /> */}
+            <div className="p-7 my-auto pb-10 ">
+              <h3 className="text-2xl font-semibold text-gray-800">
+                บันทึกการสร้างแป้นพิมพ์ไทย Manoonchai (3) : วิเคราะห์แป้นพิมพ์
+                Manoonchai เทียบกับเกษมณี,ปัตตโชติ และเปิดตัวเว็บ Manoonchai.com
+              </h3>
             </div>
           </a>
         </div>
