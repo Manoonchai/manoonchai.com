adb11058b64ee374eb628264c0ce8eaa621ddfdb
ronnapatp <ronnapat.sri@gmail.com>
2021-10-21 23:04:52 +0700

    Add linux installation , separate download page

---
+++ pages/download.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+import Download from 'components/Download';
+
+const DownloadPage: NextPage = () => {
+  return (
+    <div className="container min-h-screen mx-auto">
+      
+      <Download />
+
+      
+    </div>
+  );
+};
+
+export default DownloadPage;
